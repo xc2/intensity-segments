e3ee9f4ffa3c6c2baebfbb30ea8184ad067773d0
xc2 <gh@109cafe.com>
2024-11-29 23:02:29 +0800

    test: rename test cases and add more comments to elaborate tests

--- tests/intensity-segments.test.ts
+++ tests/intensity-segments.test.ts
@@ -4,13 +4,13 @@ import { pointNode, pointNodesToSegmentsJson } from "./helpers";
 import { simpleAddCases } from "./intensity-segments.fixtures";
 
 describe("IntensitySegments", () => {
-  test("initial value", () => {
+  test("initial value and basic case", () => {
     const segments = new IntensitySegments();
     expect(segments.toString()).toBe("[]");
     segments.add(10, 20, 1);
     expect(segments.toString()).toBe("[[10,1],[20,0]]");
   });
-  test.each(Object.keys(simpleAddCases))("#add %s", (key) => {
+  test.each(Object.keys(simpleAddCases))("#add: %s", (key) => {
     const segments = new IntensitySegments();
     const c = simpleAddCases[key];
     for (const args of c.actions) {
@@ -18,31 +18,35 @@ describe("IntensitySegments", () => {
     }
     expect(segments.toString()).toBe(pointNodesToSegmentsJson(c.result));
   });
-  test("merge: boundary", () => {
+  test("zero-intensity point of left boundary should be removed", () => {
     const segments = new IntensitySegments();
     segments.add(10, 30, 1);
     segments.add(20, 40, 1); // [[10,1],[20,2],[30,1],[40,0]]
-    segments.add(10, 40, -1);
+    segments.add(10, 40, -1); // [[10,0],[20,1],[30,0],[40,0]]
+    //                             ^^^^                ^^^^^ unnecessary
     expect(segments.toString()).toBe(
       pointNodesToSegmentsJson([pointNode(20, 1), pointNode(30, 0)])
     );
   });
 
-  test("merge: inside", () => {
+  test("unnecessary points inside should be removed", () => {
     const segments = new IntensitySegments();
     segments.add(10, 40, 2);
     segments.add(20, 30, 1); // [[10,2],[20,3],[30,2],[40,0]]
-    segments.add(20, 30, -1);
+    segments.add(20, 30, -1); // [[10,2],[20,2],[30,2],[40,0]]
+    //                                   ^^^^^^^^^^^^^ unnecessary
     expect(segments.toString()).toBe(
       pointNodesToSegmentsJson([pointNode(10, 2), pointNode(40, 0)])
     );
   });
 
-  test("#set", () => {
+  // most cases are covered by `#add: %s`, so we only test the edge cases for `#set` here
+  test("#set: basic", () => {
     const segments = new IntensitySegments();
     segments.add(10, 40, 2);
     segments.add(20, 30, 1); // [[10,2],[20,3],[30,2],[40,0]]
-    segments.set(20, 30, 2);
+    segments.set(20, 30, 2); // [[10,2],[20,2],[30,2],[40,0]]
+    //                                  ^^^^^ [20,30) is set to `2`
     expect(segments.toString()).toBe(
       pointNodesToSegmentsJson([pointNode(10, 2), pointNode(40, 0)])
     );
@@ -52,7 +56,8 @@ describe("IntensitySegments", () => {
     const segments = new IntensitySegments();
     segments.add(10, 40, 2);
     segments.add(20, 30, 1); // [[10,2],[20,3],[30,2],[40,0]]
-    segments.set(10, 40, 1);
+    segments.set(10, 40, 1); // [[10,1],[20,1],[30,1],[40,0]]
+    //                           ^^^^^^^^^^^^^^^^^^^ [10,40) is set to `1`
     expect(segments.toString()).toBe(
       pointNodesToSegmentsJson([pointNode(10, 1), pointNode(40, 0)])
     );
